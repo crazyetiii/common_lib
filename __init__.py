d3d67cfed2d73c8c932519551de22b605f661eb2
crazyetiii <50905019+crazyetiii@users.noreply.github.com>
2024-11-05 13:43:55 +0800

    1.移除setup.py的方式 2.添加部分类库方法

---
+++ __init__.py
@@ -0,0 +1,6 @@
+# 默认导入 cus_log 包中的所有包接口
+from .cus_log.cus_log import *
+from .cus_path.cus_path import *
+from .cus_pyside2.cus_pyside2 import *
+from .cus_subprocess.cus_subprocess import *
+from .cus_config import *
